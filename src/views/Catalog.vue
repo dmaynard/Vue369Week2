bcf604e9f2dae68b186a560e91bd58fa499ccf97
dmaynard <davidsmaynard@gmail.com>
2020-02-22 15:22:28 -0800

    logo changed

---
+++ src/views/Catalog.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Maynard's Juggling Emporium</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      product: "",
+      checkNames: []
+    };
+  }
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/Logo.png" />
+
+    <Catalog msg="Maynard's Juggling Emporium" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+import Catalog from "@/views/Catalog.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Catalog
   }
 };
 </script>
